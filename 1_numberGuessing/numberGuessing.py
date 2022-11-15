e8221084a7a33f6e24c1eec1eb5b91e407123a30
Piyush Jain <piyushjain9399@gmail.com>
2022-11-15 18:00:25 +0530

    1st Project: NumberGuessing python game app Started with numberGuessing python project. Added the idea and flow of the project to the numberGuessing.py file.

---
+++ 1_numberGuessing/numberGuessing.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+
+# Idea -> A number guessing python app that generates a random number from 1 to 100 and asks the user to guess the generated number in minimum number of attempts.
+
+# Flow ->   1. Generating a random number.
+#           2. Taking inputs from user continually and storing the number of attempts the user is making.
+#           3. Once the user guesses the number, print out the number of attempts taken.
+
+# 0. Importing reqd libraries.
+import random
+
+def generateRandom():
+    """
+    Function to generate and return a random integer from 1 to 100
+    """
+    randNum = random.randInt(1,100)
+    return randNum
+
+
