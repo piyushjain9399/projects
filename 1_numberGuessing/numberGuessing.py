be85cb439e3e17d4c54da6b0a2c7a8c239b9022f
Piyush Jain <piyushjain9399@gmail.com>
2022-11-16 00:23:00 +0530

    Added highScore functionality to the app 1. Including the feature to compare against the highScore which is    maintained in a highScore.txt file in the same directory. 2. Updated the README.txt with this update.

--- 1_numberGuessing/numberGuessing.py
+++ 1_numberGuessing/numberGuessing.py
@@ -10,8 +10,14 @@
 #               or less then their guess and storing the number of attempts the user is making.
 #           3. Once the user guesses the number, print out the number of attempts taken.
 
+
+# V2    ->  Adding the feature to store high scores. 
+#           i.e. writing the high score to a file stored in the same directory and updating the score
+#           if the current score beats the high score.
+
 # 0. Importing reqd libraries.
 import random
+import os
 
 # 1. Generating random number
 def generateRandom():
@@ -28,6 +34,14 @@ def getInput():
     """
     guess = int(input("Make a guess between 1 to 100: "))
     return guess
+
+# Function to check if highScore.txt file exists
+def checkHighScoreFile():
+    path = os.getcwd()
+    path += "/highScore.txt"
+    isFile = os.path.isfile(path)
+    return isFile
+
 
 # Defining the main function and writing the call for it.
 def main():
@@ -48,6 +62,24 @@ def main():
         guessReqd += 1
     print("You've guessed the correct number which is:", ans)
     print(f"You took {guessReqd} guesses.")
+    
+    highScoreFileExists = checkHighScoreFile()
+
+    if highScoreFileExists:
+        file1 = open(r"highScore.txt", "r")
+        highScore = int(file1.read())
+        
+        # TODO: Updating the highScore.txt with the better highScore.
+        if guessReqd < highScore:
+
+            print(f"Congratulations! You have set up a new highScore with {guessReqd} guesses")
+
+        else:
+            print("The highscore is:", highScore)
+    else:
+        file1 = open(r"highScore.txt", "w")
+        file1.write(str(guessReqd))
+        print("Congratulations! You have set up a new highScore!")
 
 if __name__ == "__main__":
     main()
