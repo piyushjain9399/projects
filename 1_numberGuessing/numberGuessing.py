b5f6f7ba1740fe291549ce10eadec8c5ccabf965
Piyush Jain <piyushjain9399@gmail.com>
2022-11-15 21:21:37 +0530

    Corrected the idea and flow of app 1. Corrected the concept idea behind the app. 2. Modified the code with the modified idea.

--- 1_numberGuessing/numberGuessing.py
+++ 1_numberGuessing/numberGuessing.py
@@ -1,9 +1,13 @@
 #! /usr/bin/env python3
 
-# Idea -> A number guessing python app that generates a random number from 1 to 100 and asks the user to guess the generated number in minimum number of attempts.
+# Idea ->   A number guessing python app that generates a random number from 1 to 100 
+#           and asks the user to guess the generated number in minimum number of attempts.
+#           After each attempt user is told if the number to be guessed is smaller than or 
+#           greater than his/her/their guess.
 
 # Flow ->   1. Generating a random number.
-#           2. Taking inputs from user continually and storing the number of attempts the user is making.
+#           2. Taking inputs from user continually, telling user if ans is greater than 
+#               or less then their guess and storing the number of attempts the user is making.
 #           3. Once the user guesses the number, print out the number of attempts taken.
 
 # 0. Importing reqd libraries.
@@ -35,6 +39,12 @@ def main():
     ans = generateRandom()
     while (guess != ans):
         guess = getInput()
+        if guess == ans:
+            break
+        elif guess < ans:
+            print("The correct ans is greater than your guess.")
+        else:
+            print("The correct ans is smaller than your guess.")
         guessReqd += 1
     print("You've guessed the correct number which is:", ans)
     print(f"You took %{guessReqd} guesses.")
